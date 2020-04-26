95700e2a8b387211726ce80b7e2f74efb6ccd90b
Rana Ahmed <rana.ahmed99000@gmail.com>
2020-04-26 17:27:52 +0200

    Change session contoller response to jbuilder view.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::SessionsController < ActionController::API
   def create
-    user = User.find_by(username: params[:username])
-    if user.try(:authenticate, params[:password])
-      session[:user_id] = user.id
-      render :json => user, status: :ok
+    @user = User.find_by(username: params[:username])
+    if @user.try(:authenticate, params[:password])
+      session[:user_id] = @user.id
+      render :template => "api/v1/users/show", status: :ok
     else
-      render status: :unauthorized
+      render :template => "api/v1/empty", status: :unauthorized
     end
   end
 
   def destroy
     session[:user_id] = nil
-    render status: :ok
+    render :template => "api/v1/empty", status: :ok
   end
 end
