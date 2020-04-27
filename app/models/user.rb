c3a0f97a33a91b285e06e6d645d229bd9ff9a1aa
Rana Ahmed <rana.ahmed99000@gmail.com>
2020-04-27 22:55:52 +0200

    Add devise to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
   validates :email, :name, presence: true
-  has_secure_password
   has_many :articles, dependent: :destroy
 end
