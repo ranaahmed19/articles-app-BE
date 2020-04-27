7d4a848f78d13f3dde065d4c109eba894e4bcb5c
Rana Ahmed <rana.ahmed99000@gmail.com>
2020-04-27 23:00:11 +0200

    Overwite sessions and regristration devise contollers

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,9 @@
-class Api::V1::SessionsController < ActionController::API
+class Api::V1::SessionsController < Devise::SessionsController
+  # before_action :authenticate_user!
+
   def create
-    @user = User.find_by(username: session_params[:username])
-    if @user.try(:authenticate, session_params[:password])
-      session[:user_id] = @user.id
-      render :template => "api/v1/users/show", status: :ok
-    else
-      render :template => "api/v1/empty", status: :unauthorized
-    end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    render :template => "api/v1/empty", status: :ok
-  end
-
-  private
-
-  def session_params
-    params.require(:user).permit(:username, :password)
+    super { |resource|
+      @user = resource
+    }
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,48 +1,7 @@
-class Api::V1::UsersController < ActionController::API
-  before_action :set_user, only: %i[show update destroy]
-
-  def index
-    @users = User.all
-    render status: :ok
-  end
-
-  def show
-    render status: :ok
-  end
-
+class Api::V1::UsersController < Devise::RegistrationsController
   def create
-    @user = User.new(user_param)
-    if @user.save
-      session[:user_id] = @user.id
-      render :template => "api/v1/users/show", status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @user.update(user_param)
-    if @user.save
-      render :template => "api/v1/users/show", status: :ok
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user.destroy
-    render :template => "api/v1/empty", status: :ok
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render :template => "api/v1/empty", status: :not_found
-  end
-
-  def user_param
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    super { |resource|
+      @user = resource
+    }
   end
 end
